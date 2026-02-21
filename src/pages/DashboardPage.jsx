c1a0dad788b94ed29d8dd5b51cf0b996801c4cb8
youssef <youssefmoussaddeq00@gmail.com>
2026-02-21 23:23:04 +0100

    fix

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -182,8 +182,8 @@ const DashboardPage = () => {
       <main className="flex-1 flex flex-col overflow-hidden">
         <header className="h-20 bg-white/80 backdrop-blur-md border-b border-slate-100 flex items-center justify-between px-4 md:px-8 sticky top-0 z-10 pl-16 md:pl-8">
           <div>
-            <h2 className="text-xl font-bold">Overview</h2>
-            <p className="text-xs font-bold text-text-muted">{new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</p>
+            <h2 className="md:text-xl text-lg font-bold">Overview</h2>
+            <p className="text-xs font-bold text-text-muted hidden md:block">{new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</p>
           </div>
           <div className="flex items-center gap-6">
             <div className="relative hidden md:block ">
